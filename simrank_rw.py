cacad4d6b6d52dbc054a73145f51a00d9b276f96
nali <nali@nalideMacBook-Pro-2.local>
2015-07-08 11:24:25 +0800

    the last python version of simrank

--- simrank_rw.py
+++ simrank_rw.py
@@ -12,7 +12,7 @@ import random
 conf = SparkConf()
 sc = SparkContext(appName = "tag")
 
-data_extend = sc.textFile('/Users/nali/Beifei/ximalaya2015/data/tag/albumtag100.txt').map(lambda x: x.split(','))\
+data_extend = sc.textFile('/Users/nali/Beifei/ximalaya2015/data/tag/albumtag.txt').map(lambda x: x.split(','))\
        .filter(lambda x: len(x) > 1)\
        .map(lambda x: map(lambda a: (x[0], a), x[1:]))\
        .flatMap(lambda x: x)\
@@ -38,28 +38,26 @@ class Tree:
         self.nodes = map(lambda x: Node(x, [], 0), albums)
         self.nodeMap = {}
         for node in self.nodes:
-            self.nodeMap[node.id] = node
+            self.nodeMap[(node.id, 0)] = node
         
-    def connect(self, parent_id, children_ids):
+    def connect(self, parent_id, children_ids, depth):
         #connect children and parent
         for child_id in children_ids:
-            child_node = self.nodeMap[child_id]
+            child_node = self.nodeMap[(child_id, depth-1)]
             child_node.parent_id = parent_id
-        child_depth = self.nodeMap[children_ids[0]].depth
-        parent_node = self.nodeMap[parent_id]
-        parent_node.depth = child_depth + 1
-        parent_node.children_ids +=  children_ids
+        self.nodeMap[(parent_id, depth)] = Node(parent_id, children_ids, depth)
 
-    def find_all_chilren_ids(self, parent_id):
-        node = self.nodeMap[parent_id]
+    def find_all_children_ids(self, parent_id, depth):
+        node = self.nodeMap[(parent_id, depth)]
         children = node.children_ids
-        seq = range(node.depth)
-        seq.reverse()
-        for i in seq:
-            newChildren = []
-            for child in children:
-                newChildren = newChildren + self.nodeMap[child].children_ids
-            children = newChildren
+        if depth > 1:
+            seq = range(1, depth)
+            seq.reverse()
+            for i in seq:
+                newChildren = []
+                for child in children:
+                    newChildren = newChildren + self.nodeMap[(child, i)].children_ids
+                children = newChildren
         return children
 
 def move(inputNode, childrenAll):
@@ -89,172 +87,75 @@ for i in range(10):
             .filter(lambda x: x[0] != 'End')\
             .groupByKey()\
             .map(lambda x: (x[0], x[1].data))
-
-    #        .map(lambda x: (x[0], flatten(x[1].data)))\
-    #        .filter(lambda x: len(x) == 2 & len(x[1]) != 0)\
-
-    step.foreach(lambda x: tree.connect(x[0], x[1]))
-
-    for i in range(1,10):
+    step_coll = step.collect()
+    for each in step_coll:
+        tree.connect(each[0], each[1], 1)
+#    step.foreach(lambda x: tree.connect(x[0], x[1]))
+    for j in range(2,11):
         step = step.map(lambda x: (move(x[0], x[1]+[x[0]]), x))\
                 .filter(lambda x: x[0] != 'End')\
                 .groupByKey()\
                 .map(lambda x: (x[0], x[1].data))
-        step.map(lambda x: (x[0], update(x[1]))).foreach(lambda x: tree.connect(x[0], x[1]))
+        step_coll = step.map(lambda x: (x[0], update(x[1]))).collect()
+        for each in step_coll:
+            tree.connect(each[0], each[1], j)
+#        step.map(lambda x: (x[0], update(x[1]))).foreach(lambda x: tree.connect(x[0], x[1]))
         step = step.map(lambda x: (x[0], flatten(x[1])))
     trees.append(tree)
+
+for tree in trees:
+    print len(tree.nodeMap.keys())
 
 def query(album, trees, topK):
     sim = {}
     for tree in trees:
+        #calculate the similarities in the 1st level
         neighbors = []
         simNeighbor = 0.8
-        nodeMap = tree.nodeMap
-        node = nodeMap[album]
+        node = tree.nodeMap[(album, 0)] #get the node object
         parent_id = node.parent_id
         if parent_id != None:
-            ancestor = nodeMap[parent_id]
-            neighbors = list(set(ancestor.children)-set(album))
+            #get the ancestor object
+            ancestor = tree.nodeMap[(parent_id, 1)]              
+            #get the 1st level neighbors
+            neighbors = list(set(ancestor.children_ids)-set(album))
             if len(neighbors) != 0:
                 for neighbor in neighbors:
                     if neighbor in sim.keys():
-                        sim[neighbor] = sim[neighbor] + simNeighbor
+                        sim[neighbor] += simNeighbor
                     else:
                         sim[neighbor] = simNeighbor
             node = ancestor
-            for i in range(1, 10):
-                simNeighbor = 0.8*(i+1)
+            for i in range(2,11):
+                simNeighbor = 0.8**i
                 neighbors = []
                 subVertices = []
                 parent_id = node.parent_id
                 if parent_id != None:
-                    ancestor = nodeMap[parent_id]
+                    #get the ancestor object 
+                    ancestor = tree.nodeMap[(parent_id, i)]
+                    #omit the vertex which has been detected
                     subVertices = list(set(ancestor.children_ids)-set(node.id))
-                    for vertex in subVertices:
-                       neighbors = neighbors + tree.find_all_children_ids(vertex)
+                    if len(subVertices) != 0:
+                        for vertex in subVertices:
+                           neighbors = neighbors + tree.find_all_children_ids(vertex, i-1)
                     if len(neighbors) != 0:
                         for neighbor in neighbors:
                             if neighbor in sim.keys():
-                                sim[neighbor] = sim[neighbor] + simNeighbor
+                                sim[neighbor] += simNeighbor
                             else:
                                 sim[neighbor] = simNeighbor
                     node = ancestor
                 else:
                     break
     if sim != {}:
-        sim = list(np.array(sorted(sim.items(), key=operator.itemgetter(1)))[:,0][:topK])
+        sim = list(np.array(sorted(sim.items(), key=operator.itemgetter(1), reverse=True))[:,0][:topK])
     else:
         sim = []
     return (album, sim)
 
+for album in als:
+    print query(album, trees, 10)
+
 topSearch = als_sc.map(lambda x: query(x, trees, 10))
-topSearch.map(lambda x: x[0]+","+",".join(x[1])).saveAsTextFile("/Users/nali/Beifei/ximalaya2015/code_ximalaya/code_simrank/rw/topSearch100")
-
-sys.exit()
-
-def constructTree():
-    iterations = []
-    step = als_sc.map(lambda x: (move(x, al_tag_dict, []),x))\
-            .filter(lambda x: x != "End")\
-            .groupByKey()\
-            .map(lambda x: (x[0], flatten(x[1].data), flatten(x[1].data), [x[0]]))
-    tmp = step.collect()
-    nodes = []
-    for i in range(len(tmp)):
-        node = Node(tmp[i][0], tmp[i][1], 0)
-        nodes.append(node)
-    iterations.append(nodes)
-
-    for i in range(1,10):
-        if i%2 != 0:
-            inputDict = tag_al_dict
-        else:
-            inputDict = al_tag_dict
-        step = step.map(lambda x: (move(x[0], inputDict,x[2]), x))\
-                .filter(lambda x: x[0] != "End")\
-                .groupByKey()\
-                .map(lambda x: (x[0], x[1].data))\
-                .map(lambda x: (x[0], update(x[1])))\
-                .map(lambda x: (x[0], x[1][0], x[1][1], x[1][2] + [x[0]]))
-
-        tmp = step.collect()
-        nodes = []
-        for j in range(len(tmp)):
-            node = Node(tmp[j][0], tmp[j][1], i)
-            nodes.append(node)
-        iterations.append(nodes)
-    return iterations
-
-fingerPrints = []
-for i in range(100):
-    fingerPrints.append(constructTree())
-
-def query(x, fingerprint, topK):
-    def getNodeClass(node, tree, depth):
-        allNodes = tree[depth]
-        for eachNode in allNodes:
-            if eachNode.id == node:
-                break
-        return eachNode
-    def searchForVertices(node, tree, depth):
-        nodeClass = getNodeClass(node, tree, depth)
-        children = nodeClass.children
-        seq = range(depth)
-        seq.reverse()
-        for i in seq:
-            newChildren = []
-            for child in children:
-                newChildren = newChildren + getNodeClass(child, tree, i).children
-            children = newChildren
-        return children
-
-    def searchForAncestor(node, tree, depth):
-        nextNodes = tree[depth]
-        ancestor = "End"
-        for eachNextNode in nextNodes:
-            if node in eachNextNode.children:
-                ancestor = eachNextNode
-                break
-        return ancestor
-    
-    sim = {}
-    for i in range(100):
-        iteration = fingerprint[i]
-        subVertices = []
-        neighbors = []
-        simNeighbor = 0.8
-        ancestor = searchForAncestor(x, iteration, 0)
-        neighbors = list(set(ancestor.children)-set(x))
-        if len(neighbors) != 0:
-            for neighbor in neighbors:
-                if neighbor in sim.keys():
-                    sim[neighbor] = sim[neighbor] + simNeighbor
-                else:
-                    sim[neighbor] = simNeighbor
-        node = ancestor
-        for i in range(1, 10):
-            simNeighbor = 0.8**(i+1)
-            neighbors = []
-            subVertices = []
-            ancestor = searchForAncestor(node.id, iteration, i)
-            if ancestor != "End":
-                subVertices = list(set(ancestor.children)-set(node.id))
-                for vertex in subVertices:
-                    neighbors = neighbors + searchForVertices(vertex, iteration, i)
-                if len(neighbors) != 0:
-                    for neighbor in neighbors:
-                        if neighbor in sim.keys():
-                            sim[neighbor] = sim[neighbor] + simNeighbor
-                        else:
-                            sim[neighbor] = simNeighbor
-                node = ancestor
-            else:
-                break
-    if sim != {}:
-        sim = list(np.array(sorted(sim.items(), key=operator.itemgetter(1)))[:,0][:topK])
-    else:
-        sim = []
-    return (x, sim)
-
-topSearch = als_sc.map(lambda x: query(x, fingerPrints, 10))
-topSearch.map(lambda x: x[0]+","+",".join(x[1])).saveAsTextFile("/Users/nali/Beifei/ximalaya2015/code_ximalaya/code_simrank/topSearch5000")
+topSearch.map(lambda x: x[0]+","+",".join(x[1])).saveAsTextFile("/Users/nali/Beifei/ximalaya2015/code_ximalaya/code_simrank/rw/topSearch")
